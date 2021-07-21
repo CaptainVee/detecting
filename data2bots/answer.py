3324109673fc949078c81ca9d2debfcd9dedc94b
unknown <captainvee3@gmail.com>
2021-07-21 02:12:25 +0100

    finished the task

---
+++ data2bots/answer.py
@@ -0,0 +1,25 @@
+
+#importing libraries
+import pandas as pd
+import datetime
+
+
+#importing dataset
+dataset = pd.read_csv('dataset.csv')
+
+#Initializing some important variables
+expired_date = str(datetime.date(2021, 1, 1))
+dates = dataset['date']
+boolean_field = []
+
+#Check if date is expired
+for date in dates:
+    if date > expired_date:
+        boolean_field.append(False)
+    else:
+        boolean_field.append(True)
+
+dataset['obsolete'] = boolean_field
+
+#converting to json and storing in local directory
+dataset.to_json (r'C:\Users\CAPTAIN VEE\Desktop\042AI\interview\Data2bots\new_data.json')    
